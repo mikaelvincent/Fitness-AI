67b0fe67bd348c65dbef9ed5afef576f1588329b
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-11-20 01:25:03 +0800

    [UPDATE] Set up Shadcn

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const MainLayout: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout: React.FC = () => {
         <h1 className="font-poppins font-bold text-2xl text-orange">
           Main Layout
         </h1>
+        <Button>Click me</Button>
       </header>
       {/* Renders the nested routes */}
       <main>
