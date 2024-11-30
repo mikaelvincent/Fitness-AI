ee57867b1ae064d28a0aa4fd82b9ccf37f8f4bdc
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-11-30 15:19:19 +0800

    [Update] Removed Token Input Field

--- frontend/src/components/authentication/forms/ResetPasswordForm.tsx
+++ frontend/src/components/authentication/forms/ResetPasswordForm.tsx
@@ -72,23 +72,6 @@ const LoginForm = ({
                         />
                         <FormField
                             control={form.control}
-                            name="token"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Token</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            type="text"
-                                            placeholder="token1232321"
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
                             name="password"
                             render={({field}) => (
                                 <FormItem>
