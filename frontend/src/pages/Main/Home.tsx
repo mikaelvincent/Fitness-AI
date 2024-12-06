3679934f5c9e01cd89f4856bd92b318845de084d
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-07 04:21:55 +0800

    style: Remove unnecessary background class from Calendar and Home components
    Removes the `bg-background` class from the Calendar component and adds padding to the Home component for better layout.  No functional changes.

--- frontend/src/pages/Main/Home.tsx
+++ frontend/src/pages/Main/Home.tsx
@@ -4,7 +4,7 @@ import Calendar from "@/components/dashboard/Calendar.tsx";
 const Home = () => {
 
     return (
-        <div className="flex flex-col w-full h-full space-y-8">
+        <div className="flex flex-col w-full h-full space-y-8 xl:px-24 2xl:px-32 border-r-2">
             <Calendar/>
         </div>
     );
