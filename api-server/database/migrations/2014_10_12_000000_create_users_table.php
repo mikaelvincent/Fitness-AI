9897c9519ee06cd67aed499c4583d65217a6776b
T-jay Abunales <tjabunales@gmail.com>
2024-12-09 23:20:31 +0800

    fix: Set default value for email_verified_at timestamp

--- api-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ api-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string("name");
             $table->string("email")->unique();
-            $table->timestamp("email_verified_at");
+            $table->timestamp("email_verified_at")->useCurrent();
             $table->string("password");
             $table->timestamps();
         });
