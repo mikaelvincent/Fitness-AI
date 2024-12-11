794f1e911a4293ba2b4a10b3b2f5bb75fa53b598
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-12 03:51:27 +0800

    feat: Add user profile information to Profile page
    Add user profile information to the Profile page,
    including name, email, age, weight, and height.This
    information is displayed using the `ProfileDashboardUI`
    component.  The age is dynamically calculated using the
    `calculateAge` utility function.
    
    Signed-off-by: Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>

--- frontend/src/pages/Main/Profile.tsx
+++ frontend/src/pages/Main/Profile.tsx
@@ -1,9 +1,20 @@
 import ProfileDashboardUI from "@/components/profile/ProfileDashboardUI.tsx";
+import { UserProfileInfo } from "@/types/UserProfileInfo.ts";
+import { useState } from "react";
+import { calculateAge } from "@/utils/calculateAge.ts";
 
 const Profile = () => {
+  const [profileInfo, setProfileInfo] = useState<UserProfileInfo>({
+    name: "Jericho",
+    email: "pascojericho@gmail.com",
+    age: calculateAge(new Date("2000-12-31")),
+    weight: 65,
+    height: 1.7,
+  });
+
   return (
     <>
-      <ProfileDashboardUI />
+      <ProfileDashboardUI profileInfo={profileInfo} />
     </>
   );
 };
