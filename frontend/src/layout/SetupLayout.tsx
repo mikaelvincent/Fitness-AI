21916fcf33652c71b185a04966a7e155d4848983
T-jay Abunales <tjabunales@gmail.com>
2024-12-12 00:07:26 +0800

    refactor(header): remove logo and light/dark mode toggle from header.

--- frontend/src/layout/SetupLayout.tsx
+++ frontend/src/layout/SetupLayout.tsx
@@ -11,20 +11,7 @@ const SetupLayout = () => {
     const logoTheme = isLargeScreen ? "dark" : appliedTheme;
 
     return (
-        <div className="min-h-screen flex text-foreground flex-col gap-10 md:gap-40 lg:gap-0">
-            <header className="flex-none flex flex-col-reverse justify-center items-center lg:flex-row lg:justify-between pt-6 lg:p-6 w-full bg-background">
-
-                <Logo
-                    className={isLargeScreen ? "w-14 lg:w-20" : "w-60 lg:w-96"}
-                    alt="Company Logo"
-                    toUseTheme={logoTheme}
-                    variant={isLargeScreen ? "withoutText" : "withText"}
-                />
-                <div className="flex-none flex pr-2 justify-end w-full lg:w-auto">
-                    <ModeToggle />
-                </div>
-            </header>
-
+        <div className="min-h-screen flex text-foreground items-center gap-10 md:gap-40 lg:gap-0">
             <main className="flex-auto flex w-full items-start justify-center lg:items-start lg:flex-col">
                 <div className="flex-1 items-start w-full justify-center h-full p-8">
                     <Outlet />
