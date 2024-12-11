b995553d1402f805a13dc9314238723ab12c453d
T-jay Abunales <tjabunales@gmail.com>
2024-12-12 00:53:34 +0800

    refactor(layout): remove unused logo and theme toggle from SetupLayout

--- frontend/src/layout/SetupLayout.tsx
+++ frontend/src/layout/SetupLayout.tsx
@@ -1,15 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Logo from "@/components/ui/logo";
-import { ModeToggle } from "@/components/theme/ModeToggle";
-import { useTheme } from "@/components/theme/theme-provider";
-import useIsLargeScreen from "@/hooks/useIsLargeScreen";
 
 const SetupLayout = () => {
-    const { appliedTheme } = useTheme();
-    const isLargeScreen = useIsLargeScreen(1024);
-
-    const logoTheme = isLargeScreen ? "dark" : appliedTheme;
-
     return (
         <div className="min-h-screen flex text-foreground items-center gap-10 md:gap-40 lg:gap-0">
             <main className="flex-auto flex w-full items-start justify-center lg:items-start lg:flex-col">
