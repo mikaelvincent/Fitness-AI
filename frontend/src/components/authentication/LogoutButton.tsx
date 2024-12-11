f858a8919ca5457deb43c59816e3ffb015e3fb40
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-12 04:51:35 +0800

    feat: Implement logout functionality with error handling
    This commit adds a fully functional logout button that handles successful and unsuccessful logout scenarios.  It includes error handling,  a cooldown timer for rate limiting, and user feedback via toasts.  The user is redirected to the login page upon successful logout.
    
    Signed-off-by: Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>

--- frontend/src/components/authentication/LogoutButton.tsx
+++ frontend/src/components/authentication/LogoutButton.tsx
@@ -1,9 +1,62 @@
 import { Button } from "@/components/ui/button.tsx";
 import { LogOut } from "lucide-react";
+import { useUser } from "@/hooks/context/UserContext.tsx";
+import { logoutUser } from "@/services/auth/logout.ts";
+import useStatus from "@/hooks/useStatus.tsx";
+import { toast } from "@/hooks/use-toast";
+import useTimer from "@/hooks/useTimer.tsx";
+import { useNavigate } from "react-router-dom";
 
 function LogoutButton() {
-  const handleLogout = () => {
-    // Add logout logic here
+  const { token, logoutUser: contextLogoutUser } = useUser();
+  const { status, setLoading, setDone, setError } = useStatus();
+  const navigate = useNavigate();
+
+  const {
+    timeLeft: cooldown,
+    start: startCooldown,
+    reset: resetCooldown,
+  } = useTimer(
+    0,
+    () => {
+      setDone();
+    },
+    "logoutRetryTimer",
+  );
+
+  const handleLogout = async () => {
+    try {
+      const response = await logoutUser(token);
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Optional delay
+
+      if (!response?.success) {
+        setError();
+        toast({
+          title: "Success",
+          description:
+            response?.message || response.status === 429
+              ? "Too many attempts. Please try again later."
+              : "Logout failed. Please Try again",
+        });
+        startCooldown(response?.retry_after || 0);
+        return;
+      }
+      if (response?.success) {
+        contextLogoutUser();
+        toast({
+          title: "Success",
+          description: response?.message || "Logout successful",
+        });
+        navigate("/login");
+        return;
+      }
+    } catch (error) {
+      setError();
+      toast({
+        title: "Error",
+        description: "An unexpected error occurred",
+      });
+    }
   };
   return (
     <Button
