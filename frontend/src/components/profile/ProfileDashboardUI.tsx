b5b56ddadf177f562bda4add1954d745fade6366
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-12 23:58:59 +0800

    Revert "feat: Enhance profile page and 2FA flow"
    This reverts commit fd34b4ceb86bea1ca419b666faeefa678b6824e2.

--- frontend/src/components/profile/ProfileDashboardUI.tsx
+++ frontend/src/components/profile/ProfileDashboardUI.tsx
@@ -1,11 +1,5 @@
-import {
-  BotMessageSquare,
-  Edit,
-  KeyRound,
-  Save,
-  Shield,
-  X,
-} from "lucide-react";
+import { BotMessageSquare, KeyRound, Shield } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
@@ -13,8 +7,6 @@ import { ModeToggle } from "@/components/theme/ModeToggle.tsx";
 import { UserProfileInfo } from "@/types/UserProfileInfo";
 import LogoutButton from "@/components/authentication/LogoutButton";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
 
 interface ProfileDashboardUIProps {
   profileInfo: UserProfileInfo;
@@ -25,82 +17,20 @@ export default function ProfileDashboardUI({
   profileInfo,
   handleNavigation,
 }: ProfileDashboardUIProps) {
-  const [isUpdateName, setIsUpdateName] = useState<boolean>(false);
-
   return (
     <div className="flex h-full w-full flex-col justify-center gap-8 p-4 lg:p-8">
       <div className="flex justify-end">
         <ModeToggle />
       </div>
-      <div className="flex w-full gap-2">
-        {/* Profile Section */}
-        <Card className="w-1/3 flex-none border-zinc-100 bg-gray-50 dark:border-zinc-800 dark:bg-zinc-900">
-          <CardContent className="flex h-full flex-col justify-between p-6">
-            <div className="flex flex-1 flex-col justify-center space-y-2">
-              {!isUpdateName && (
-                <div className="flex justify-between">
-                  <h1 className="text-2xl font-bold">{profileInfo.name}</h1>
-                  <Button variant="ghost" onClick={() => setIsUpdateName(true)}>
-                    <Edit className="text-primary" />
-                  </Button>
-                </div>
-              )}
-              {isUpdateName && (
-                <div className="flex justify-between">
-                  <Input
-                    type="text"
-                    className="text-2xl font-bold"
-                    value={profileInfo.name}
-                    onChange={(e) => console.log(e.target.value)}
-                  />
-                  <div className="flex">
-                    <Button
-                      variant="ghost"
-                      onClick={() => setIsUpdateName(false)}
-                    >
-                      <Save className="text-primary" />
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      onClick={() => setIsUpdateName(false)}
-                    >
-                      <X className="text-red-500" />
-                    </Button>
-                  </div>
-                </div>
-              )}
-
+      {/* Profile Section */}
+      <Card className="border-zinc-100 bg-gray-50 dark:border-zinc-800 dark:bg-zinc-900">
+        <CardContent className="p-6">
+          <div className="space-y-6">
+            <div className="space-y-2">
+              <h1 className="text-2xl font-bold">{profileInfo.name}</h1>
               <p className="text-muted-foreground">{profileInfo.email}</p>
             </div>
-            <div className="space-y-2">
-              <Button
-                variant="ghost"
-                className="w-full justify-start text-lg hover:bg-secondary hover:text-primary"
-                onClick={() => handleNavigation("change-password")}
-              >
-                <div className="mr-4 rounded-full bg-primary p-2">
-                  <KeyRound className="h-5 w-5" />
-                </div>
-                Change Password
-              </Button>
-              <Button
-                variant="ghost"
-                className="w-full justify-start text-lg hover:bg-secondary hover:text-primary"
-                onClick={() => handleNavigation("two-factor-authentication")}
-              >
-                <div className="mr-4 rounded-full bg-primary p-2">
-                  <Shield className="h-5 w-5" />
-                </div>
-                Two-Factor Authentication
-              </Button>
-              <Separator className="my-4 bg-zinc-800" />
-              <LogoutButton />
-            </div>
-          </CardContent>
-        </Card>
-        {/* Actions Section */}
-        <Card className="flex-1 border-zinc-100 bg-gray-50 text-center dark:border-zinc-800 dark:bg-zinc-900">
-          <CardContent className="p-6">
+
             {/* Stats Grid - Vertical on mobile, horizontal on desktop */}
             <div className="flex-col">
               <div className="flex w-full flex-col rounded-lg bg-primary">
@@ -121,14 +51,43 @@ export default function ProfileDashboardUI({
                 <Button variant="link" size="lg">
                   <Link to="/chat" className="flex items-center gap-1">
                     <BotMessageSquare />
-                    Chat with GENIE to update your attributes
+                    Chat with Genie to Edit Attributes
                   </Link>
                 </Button>
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </div>
+          </div>
+        </CardContent>
+      </Card>
+      {/* Actions Section */}
+      <Card className="border-zinc-100 bg-gray-50 text-center dark:border-zinc-800 dark:bg-zinc-900">
+        <CardContent className="p-6">
+          <div className="space-y-2">
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-lg hover:bg-secondary hover:text-primary"
+              onClick={() => handleNavigation("change-password")}
+            >
+              <div className="mr-4 rounded-full bg-primary p-2">
+                <KeyRound className="h-5 w-5" />
+              </div>
+              Change Password
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start text-lg hover:bg-secondary hover:text-primary"
+              onClick={() => handleNavigation("two-factor-authentication")}
+            >
+              <div className="mr-4 rounded-full bg-primary p-2">
+                <Shield className="h-5 w-5" />
+              </div>
+              Two-Factor Authentication
+            </Button>
+            <Separator className="my-4 bg-zinc-800" />
+            <LogoutButton />
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
