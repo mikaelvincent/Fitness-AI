ade8629129b245ca112539d29e849fe3e81307e1
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-13 00:55:49 +0800

    feat: Add user attributes retrieval to profile page
    This commit adds functionality to retrieve and display user attributes on the profile page.  It uses a new service call to fetch attributes and updates the UI accordingly.  No breaking changes introduced.
    
    Signed-off-by: Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>

--- frontend/src/pages/Main/Profile/Profile.tsx
+++ frontend/src/pages/Main/Profile/Profile.tsx
@@ -1,21 +1,50 @@
 // Profile.tsx
 import ProfileDashboardUI from "@/components/profile/ProfileDashboardUI.tsx";
 import { UserProfileInfo } from "@/types/UserProfileInfo.ts";
-import { useState } from "react";
-import { calculateAge } from "@/utils/calculateAge.ts";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { RetrieveAttributes } from "@/services/User/RetreiveAllAttributes.ts";
+import { useUser } from "@/hooks/context/UserContext.tsx";
+import { toast } from "@/hooks/use-toast.tsx";
 
 const Profile = () => {
+  const [attributes, setAttributes] = useState<object>([]);
   const [profileInfo, setProfileInfo] = useState<UserProfileInfo>({
     name: "Jericho",
     email: "pascojericho@gmail.com",
-    age: calculateAge(new Date("2000-12-31")),
-    weight: 65,
-    height: 1.7,
   });
+
+  const { token } = useUser();
 
   const [isUpdateName, setIsUpdateName] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    handleRetrieveAttributes().then((r) => r);
+  }, []);
+
+  const handleRetrieveAttributes = async () => {
+    try {
+      const response = await RetrieveAttributes({ token });
+      if (!response?.success) {
+        toast({
+          variant: "destructive",
+          title: response?.message || "Failed to retrieve attributes",
+        });
+        return;
+      }
+
+      if (response?.success && response?.data) {
+        setAttributes(response.data);
+      }
+    } catch (error) {
+      console.error("Error retrieving attributes:", error);
+      toast({
+        variant: "destructive",
+        title: "Failed to retrieve attributes",
+      });
+    }
+  };
 
   const handleNavigation = (path: string) => {
     navigate(path);
@@ -50,6 +79,7 @@ const Profile = () => {
       onCancelEditName={handleCancelEditName}
       onNameChange={handleNameChange}
       handleNavigation={handleNavigation}
+      attributes={attributes}
     />
   );
 };
