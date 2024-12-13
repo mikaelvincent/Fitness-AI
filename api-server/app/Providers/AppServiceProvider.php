cec7c6b85560c0e82c3371789e3a9048fabb3efc
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-12-14 04:05:23 +0800

    Update AppServiceProvider.php

--- api-server/app/Providers/AppServiceProvider.php
+++ api-server/app/Providers/AppServiceProvider.php
@@ -3,12 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\UserAttributeRepository;
+use App\Repositories\ActivityRepository;
+use App\Services\UserAttributeService;
+use App\Services\ActivityService;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->app->singleton(UserAttributeRepository::class, function ($app) {
+            return new UserAttributeRepository();
+        });
+
+        $this->app->singleton(ActivityRepository::class, function ($app) {
+            return new ActivityRepository();
+        });
+
+        $this->app->singleton(UserAttributeService::class, function ($app) {
+            return new UserAttributeService($app->make(UserAttributeRepository::class));
+        });
+
+        $this->app->singleton(ActivityService::class, function ($app) {
+            return new ActivityService($app->make(ActivityRepository::class));
+        });
     }
 
     public function boot()
