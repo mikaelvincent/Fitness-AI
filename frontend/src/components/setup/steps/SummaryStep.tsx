2aeac24937928e625a996edf11f158874e9ddc63
T-jay Abunales <tjabunales@gmail.com>
2024-12-18 04:01:02 +0800

    feat: Add routing to items in summary step; Add setCurrentStep prop to StepContent and SummaryStep components

--- frontend/src/components/setup/steps/SummaryStep.tsx
+++ frontend/src/components/setup/steps/SummaryStep.tsx
@@ -3,9 +3,10 @@ import { SetupData } from "@/types/setupTypes";
 
 interface SummaryStepProps {
     data: SetupData;
+    setCurrentStep: (stepId: number) => void;
 }
 
-export const SummaryStep: React.FC<SummaryStepProps> = ({ data }) => {
+export const SummaryStep: React.FC<SummaryStepProps> = ({ data, setCurrentStep }) => {
     const {
         gender,
         birthdate,
@@ -23,21 +24,23 @@ export const SummaryStep: React.FC<SummaryStepProps> = ({ data }) => {
     const displayedHeight = `${height}${heightUnit}`;
 
     const info = [
-        { label: "Gender", value: gender },
-        { label: "Birthdate", value: birthdateString },
-        { label: "Measurement", value: measurement },
-        { label: "Weight", value: displayedWeight },
-        { label: "Height", value: displayedHeight },
-        { label: "Activity Level", value: activity },
-        { label: "Nickname", value: nickname },
+        { label: "Gender", value: gender, index: 0 },
+        { label: "Birthdate", value: birthdateString, index: 1 },
+        { label: "Measurement", value: measurement, index: 2 },
+        { label: "Weight", value: displayedWeight, index: 3 },
+        { label: "Height", value: displayedHeight, index: 4 },
+        { label: "Activity Level", value: activity, index: 5 },
+        { label: "Nickname", value: nickname, index: 6 },
     ];
 
     return (
         <div className="space-y-4">
             <div className="grid gap-4">
                 {info.map((item) => (
-                    <div key={item.label} className="p-4 border rounded-lg">
-                        <strong>{item.label}:</strong> {item.value}
+                    <div key={item.label} className="p-4 border rounded-lg hover:bg-primary">
+                        <button onClick={() => setCurrentStep(item.index)} className="w-full text-left">
+                            <strong>{item.label}:</strong> {item.value}
+                        </button>
                     </div>
                 ))}
             </div>
