eb7610fb083cb04655ae69b93a06b9cc839e738c
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-17 13:38:55 +0800

    Fix: Vite config.ts updates fix
    Signed-off-by: Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,4 +16,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    exclude: ["chunk-WVXBTL6A"], // Replace with the exact dependency name
+  },
 });
