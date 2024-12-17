bdaafbf7ea6e9f0e7e587d3b3081a611265e61df
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-12-18 02:59:35 +0800

    Remove token refresh endpoint
    - Deleted TokenController.php and TokenControllerTest.php and removed the token refresh route from api.php.
    
    Token refresh functionality is obsolete due to the implementation of sliding token expiration.

--- api-server/routes/api.php
+++ api-server/routes/api.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegistrationController;
 use App\Http\Controllers\Auth\SessionController;
 use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\TokenController;
 use App\Http\Controllers\Auth\TwoFactorController;
 use App\Http\Controllers\User\UserAttributeController;
 use App\Http\Controllers\Activities\ActivityController;
@@ -26,8 +24,9 @@ Route::group([], function () {
 // Authentication Routes
 Route::post('/login', [SessionController::class, 'login'])
     ->middleware('custom.rate_limiter:authentication');
+
 Route::post('/logout', [SessionController::class, 'logout'])
-    ->middleware('auth:sanctum');
+    ->middleware(['auth:sanctum', 'update.token.expiry']);
 
 // Password Management Routes
 Route::post('/password/forgot', [PasswordController::class, 'sendResetLink'])
@@ -35,40 +34,36 @@ Route::post('/password/forgot', [PasswordController::class, 'sendResetLink'])
 Route::post('/password/reset', [PasswordController::class, 'reset'])
     ->middleware('custom.rate_limiter:password_reset');
 Route::post('/password/change', [PasswordController::class, 'change'])
-    ->middleware('auth:sanctum');
-
-// Token Refresh Route
-Route::post('/token/refresh', [TokenController::class, 'refresh'])
-    ->middleware('auth:sanctum');
+    ->middleware(['auth:sanctum', 'update.token.expiry']);
 
 // Two-Factor Authentication Routes
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum', 'update.token.expiry'])->group(function () {
     Route::post('/two-factor/enable', [TwoFactorController::class, 'enable']);
     Route::post('/two-factor/confirm', [TwoFactorController::class, 'confirm']);
     Route::post('/two-factor/disable', [TwoFactorController::class, 'disable']);
 });
 
 // User Attributes Routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'update.token.expiry'])->group(function () {
     Route::get('/user/attributes', [UserAttributeController::class, 'index']);
     Route::put('/user/attributes', [UserAttributeController::class, 'update']);
     Route::delete('/user/attributes', [UserAttributeController::class, 'destroy']);
 });
 
 // Activities Routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'update.token.expiry'])->group(function () {
     Route::get('/activities', [ActivityController::class, 'index']);
     Route::put('/activities', [ActivityController::class, 'update']);
     Route::delete('/activities', [ActivityController::class, 'destroy']);
 });
 
 // Chat Routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'update.token.expiry'])->group(function () {
     Route::post('/chat', [ChatController::class, 'handle']);
 });
 
 // User Profile Routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'update.token.expiry'])->group(function () {
     Route::get('/user/profile', [UserProfileController::class, 'show']);
     Route::put('/user/profile/name', [UserProfileController::class, 'updateName']);
 });
