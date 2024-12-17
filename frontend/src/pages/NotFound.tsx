4ad16a573e66785869cef6879d476c223c272bcd
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2024-12-18 05:31:43 +0800

    fix: Redirect to /dashboard on 404 page
    The "Go Back" button on the 404 page now redirects to the `/dashboard` route instead of the root `/` route.  This provides a more useful navigation option for users encountering a 404 error.
    
    Signed-off-by: Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,39 +1,43 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import CardWrapper from "@/components/authentication/auth-ui/CardWrapper.tsx";
-import {Button} from "@/components/ui/button.tsx";
-import {ModeToggle} from "@/components/theme/ModeToggle.tsx";
-
+import { Button } from "@/components/ui/button.tsx";
+import { ModeToggle } from "@/components/theme/ModeToggle.tsx";
 
 const NotFound = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = () => {
-        // route to dashboard
-        navigate("/");
-    }
+  const handleSubmit = () => {
+    // route to dashboard
+    navigate("/dashboard");
+  };
 
-    return (
-        <>
-            <div className="flex flex-col h-screen">
-                <div className="flex-none flex justify-end pt-6 pr-2">
-                    <ModeToggle/>
-                </div>
+  return (
+    <>
+      <div className="flex h-screen flex-col">
+        <div className="flex flex-none justify-end pr-2 pt-6">
+          <ModeToggle />
+        </div>
 
-                {/* Header for the 404 Not Found page */}
-                <div className="flex-1 h-full flex justify-center items-center">
-                    <CardWrapper label="This content isn't available right now" title="404 Not Found page" backLabel=""
-                                 backButtonHref="" backButtonLabel="" logo="logo">
-                        <div className="flex justify-center">
-                            <Button onClick={handleSubmit} className="mb-4">
-                                Go Back to Dashboard
-                            </Button>
-                        </div>
-
-                    </CardWrapper>
-                </div>
+        {/* Header for the 404 Not Found page */}
+        <div className="flex h-full flex-1 items-center justify-center">
+          <CardWrapper
+            label="This content isn't available right now"
+            title="404 Not Found page"
+            backLabel=""
+            backButtonHref=""
+            backButtonLabel=""
+            logo="logo"
+          >
+            <div className="flex justify-center">
+              <Button onClick={handleSubmit} className="mb-4">
+                Go Back to Dashboard
+              </Button>
             </div>
-        </>
-    );
+          </CardWrapper>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
